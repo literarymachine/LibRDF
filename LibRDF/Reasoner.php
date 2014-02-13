a8f80521706ec475356d8d47a54008e3df4fb639
Felix Ostrowski <felix.ostrowski@googlemail.com>
2014-02-13 16:14:00 +0100

    Partially implement RDFS+
    Add support for owl:inverseOf, owl:SymmetricProperty and
    owl:TransitiveProperty

--- LibRDF/Reasoner.php
+++ LibRDF/Reasoner.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * LibRDF_Reasoner, an RDFS inference engine
+ * LibRDF_Reasoner, an RDFS+ inference engine
  *
  * PHP version 5
  *
@@ -34,9 +34,26 @@ require_once(dirname(__FILE__) . '/Model.php');
 require_once(dirname(__FILE__) . '/Query.php');
 
 /**
- * Simple RDFS inference engine, based on SPARQL queries
+ * Simple RDFS+ inference engine, based on SPARQL queries
  */
-class LibRDF_Reasoner {
+class LibRDF_Reasoner
+{
+
+  /**
+   * The mode to use, currently 'rdfs' or 'rdfs+'
+   *
+   * @var string
+   */
+  private $__mode;
+
+  /**
+   * Create a new LibRDF_Reasoner
+   *
+   * @param  string  $mode The mode to use
+   */
+  public function __construct($mode) {
+    $this->__mode = $mode;
+  }
 
   /**
    * Add inferred (implicit) statements to instance data
@@ -44,8 +61,9 @@ class LibRDF_Reasoner {
    * @param  LibRDF_Model  $tbox The model containing the ontology
    * @param  LibRDF_Model  $abox The model containing instance data
    */
-  public function inferStatements(LibRDF_Model $tbox, LibRDF_Model $abox) {
-    $this->_inferStatements($this->buildQueries($tbox), $abox);
+  public function inferStatements(LibRDF_Model $tbox, LibRDF_Model $abox)
+  {
+    $this->_inferStatements($this->_buildQueries($tbox), $abox);
   }
 
   /**
@@ -57,7 +75,8 @@ class LibRDF_Reasoner {
    * @param  array  $abox    The instance data that the inferred triples
    *                         will be added to
    */
-  protected function _inferStatements($queries, $abox) {
+  protected function _inferStatements($queries, $abox)
+  {
     $add_count = 0;
     foreach ($queries as $query) {
       $inferred = $query->execute($abox);
@@ -76,11 +95,35 @@ class LibRDF_Reasoner {
    *
    * @param  LibRDF_Model  $tbox The ontology model
    */
-  public function buildQueries(LibRDF_Model $tbox) {
+  protected function _buildQueries(LibRDF_Model $tbox)
+  {
+    $queries = array();
+    switch ($this->__mode) {
+      case 'rdfs+':
+        $queries = array_merge($this->_buildOwlInverseOfQueries($tbox), $queries);
+        $queries = array_merge($this->_buildOwlSymmetricPropertyQueries($tbox), $queries);
+        $queries = array_merge($this->_buildOwlTransitivePropertyQueries($tbox), $queries);
+      case 'rdfs':
+      default:
+        $queries = array_merge($this->_buildRdfsSubPropertyOfQueries($tbox), $queries);
+        $queries = array_merge($this->_buildRdfsSubClassOfQueries($tbox), $queries);
+        $queries = array_merge($this->_buildRdfsDomainQueries($tbox), $queries);
+        $queries = array_merge($this->_buildRdfsRangeQueries($tbox), $queries);
+    }
+    return $queries;
+  }
+
+
+  /**
+   * Generate SPARQL CONSTRUCT queries for rdfs:subPropertyOf
+   *
+   * @param  mixed  $tbox The ontology model
+   * @return array        The generated queries
+   */
+  protected function _buildRdfsSubPropertyOfQueries($tbox)
+  {
     $RDFS = new LibRDF_NS('http://www.w3.org/2000/01/rdf-schema#');
-
-    $rdfsQueries = array();
-
+    $rdfsSubPropertyOfQueries = array();
     $rdfsSubPropertyOfStmts = $tbox->findStatements(
       null, $RDFS->subPropertyOf, null
     );
@@ -90,11 +133,23 @@ class LibRDF_Reasoner {
         $rdfsSubPropertyOfStmt->getObject(),
         $rdfsSubPropertyOfStmt->getSubject()
       );
-      $rdfsQueries[] = new LibRDF_Query(
+      $rdfsSubPropertyOfQueries[] = new LibRDF_Query(
         $rdfsSubPropertyOfQueryString, null, 'sparql'
       );
     }
+    return $rdfsSubPropertyOfQueries;
+  }
 
+  /**
+   * Generate SPARQL CONSTRUCT queries for rdfs:subClassOf
+   *
+   * @param  mixed  $tbox The ontology model
+   * @return array        The generated queries
+   */
+  protected function _buildRdfsSubClassOfQueries($tbox)
+  {
+    $RDFS = new LibRDF_NS('http://www.w3.org/2000/01/rdf-schema#');
+    $rdfsSubClassOfQueries = array();
     $rdfsSubClassOfStmts = $tbox->findStatements(
       null, $RDFS->subClassOf, null
     );
@@ -104,11 +159,23 @@ class LibRDF_Reasoner {
         $rdfsSubClassOfStmt->getObject(),
         $rdfsSubClassOfStmt->getSubject()
       );
-      $rdfsQueries[] = new LibRDF_Query(
+      $rdfsSubClassOfQueries[] = new LibRDF_Query(
         $rdfsSubClassOfQueryString, null, 'sparql'
       );
     }
+    return $rdfsSubClassOfQueries;
+  }
 
+  /**
+   * Generate SPARQL CONSTRUCT queries for rdfs:domain
+   *
+   * @param  mixed  $tbox The ontology model
+   * @return array        The generated queries
+   */
+  protected function _buildRdfsDomainQueries($tbox)
+  {
+    $RDFS = new LibRDF_NS('http://www.w3.org/2000/01/rdf-schema#');
+    $rdfsDomainQueries = array();
     $rdfsDomainStmts = $tbox->findStatements(
       null, $RDFS->domain, null
     );
@@ -118,11 +185,23 @@ class LibRDF_Reasoner {
         $rdfsDomainStmt->getObject(),
         $rdfsDomainStmt->getSubject()
       );
-      $rdfsQueries[] = new LibRDF_Query(
+      $rdfsDomainQueries[] = new LibRDF_Query(
         $rdfsDomainQueryString, null, 'sparql'
       );
     }
+    return $rdfsDomainQueries;
+  }
 
+  /**
+   * Generate SPARQL CONSTRUCT queries for rdfs:range
+   *
+   * @param  mixed  $tbox The ontology model
+   * @return array        The generated queries
+   */
+  protected function _buildRdfsRangeQueries($tbox)
+  {
+    $RDFS = new LibRDF_NS('http://www.w3.org/2000/01/rdf-schema#');
+    $rdfsRangeQueries = array();
     $rdfsRangeStmts = $tbox->findStatements(
       null, $RDFS->range, null
     );
@@ -132,12 +211,92 @@ class LibRDF_Reasoner {
         $rdfsRangeStmt->getObject(),
         $rdfsRangeStmt->getSubject()
       );
-      $rdfsQueries[] = new LibRDF_Query(
+      $rdfsRangeQueries[] = new LibRDF_Query(
         $rdfsRangeQueryString, null, 'sparql'
       );
     }
+    return $rdfsRangeQueries;
+  }
 
-    return $rdfsQueries;
+  /**
+   * Generate SPARQL CONSTRUCT queries for owl:inverseOf
+   *
+   * @param  mixed  $tbox The ontology model
+   * @return array        The generated queries
+   */
+  protected function _buildOwlInverseOfQueries($tbox)
+  {
+    $OWL = new LibRDF_NS('http://www.w3.org/2002/07/owl#');
+    $owlInverseOfQueries = array();
+    $owlInverseOfStmts = $tbox->findStatements(
+      null, $OWL->inverseOf, null
+    );
+    foreach ($owlInverseOfStmts as $owlInverseOfStmt) {
+      $owlInverseOfQueryString = sprintf(
+        "CONSTRUCT {?o %s ?s} WHERE {?s %s ?o}\n",
+        $owlInverseOfStmt->getSubject(),
+        $owlInverseOfStmt->getObject()
+      );
+      $owlInverseOfQueries[] = new LibRDF_Query(
+        $owlInverseOfQueryString, null, 'sparql'
+      );
+    }
+    return $owlInverseOfQueries;
+  }
+
+  /**
+   * Generate SPARQL CONSTRUCT queries for owl:SymmetricProperty
+   *
+   * @param  mixed  $tbox The ontology model
+   * @return array        The generated queries
+   */
+  protected function _buildOwlSymmetricPropertyQueries($tbox)
+  {
+    $OWL = new LibRDF_NS('http://www.w3.org/2002/07/owl#');
+    $RDF = new LibRDF_NS('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+    $owlSymmetricPropertyQueries = array();
+    $owlSymmetricPropertyStmts = $tbox->findStatements(
+      null, $RDF->type, $OWL->SymmetricProperty
+    );
+    foreach ($owlSymmetricPropertyStmts as $owlSymmetricPropertyStmt) {
+      $owlSymmetricPropertyQueryString = sprintf(
+        "CONSTRUCT {?o %s ?s} WHERE {?s %s ?o}\n",
+        $owlSymmetricPropertyStmt->getSubject(),
+        $owlSymmetricPropertyStmt->getSubject()
+      );
+      $owlSymmetricPropertyQueries[] = new LibRDF_Query(
+        $owlSymmetricPropertyQueryString, null, 'sparql'
+      );
+    }
+    return $owlSymmetricPropertyQueries;
+  }
+
+  /**
+   * Generate SPARQL CONSTRUCT queries for owl:TransitiveProperty
+   *
+   * @param  mixed  $tbox The ontology model
+   * @return array        The generated queries
+   */
+  protected function _buildOwlTransitivePropertyQueries($tbox)
+  {
+    $OWL = new LibRDF_NS('http://www.w3.org/2002/07/owl#');
+    $RDF = new LibRDF_NS('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+    $owlTransitivePropertyQueries = array();
+    $owlTransitivePropertyStmts = $tbox->findStatements(
+      null, $RDF->type, $OWL->TransitiveProperty
+    );
+    foreach ($owlTransitivePropertyStmts as $owlTransitivePropertyStmt) {
+      $owlTransitivePropertyQueryString = sprintf(
+        "CONSTRUCT {?s %s ?o} WHERE {?s %s ?x . ?x %s ?o}\n",
+        $owlTransitivePropertyStmt->getSubject(),
+        $owlTransitivePropertyStmt->getSubject(),
+        $owlTransitivePropertyStmt->getSubject()
+      );
+      $owlTransitivePropertyQueries[] = new LibRDF_Query(
+        $owlTransitivePropertyQueryString, null, 'sparql'
+      );
+    }
+    return $owlTransitivePropertyQueries;
   }
 
 }
